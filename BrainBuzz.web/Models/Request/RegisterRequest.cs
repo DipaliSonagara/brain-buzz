9a9bc3351dd72d711e2bac2940a80cb59b2b61e2
jalpeshmanektech <jalpeshnakummt@gmail.com>
2025-07-18 18:42:42 +0530

    added validation through data annotation

---
+++ BrainBuzz.web/Models/Request/RegisterRequest.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BrainBuzz.web.Models.Request
+{
+     public class RegisterRequest
+     {
+          [Required(ErrorMessage = "Username must be pass")]
+          public string Username { get; set; } = default!;
+          [Required]
+          public string Password { get; set; } = default!;
+          [Required]
+          public string ConfirmPassword { get; set; } = default!;
+          [Required]
+          public string PasswordHash { get; set; } = default!;
+          public string? Role { get; set; }
+          [Required]
+          public string FirstName { get; set; } = default!;
+          [Required]
+          public string LastName { get; set; } = default!;
+          public string? Email { get; set; }
+     }
+}

580ffc08eaee012b41c4671aa886ab47fec6ac62
jalpeshmanektech <jalpeshnakummt@gmail.com>
2025-07-18 19:05:51 +0530

    getting call on valid data submit

--- BrainBuzz.web/Models/Request/RegisterRequest.cs
+++ BrainBuzz.web/Models/Request/RegisterRequest.cs
@@ -5,18 +5,17 @@ namespace BrainBuzz.web.Models.Request
      public class RegisterRequest
      {
           [Required(ErrorMessage = "Username must be pass")]
-          public string Username { get; set; } = default!;
+          public string Username { get; set; } = string.Empty;
           [Required]
-          public string Password { get; set; } = default!;
+          public string Password { get; set; } = string.Empty;
           [Required]
-          public string ConfirmPassword { get; set; } = default!;
-          [Required]
-          public string PasswordHash { get; set; } = default!;
+          public string ConfirmPassword { get; set; } = string.Empty;
+          public string? PasswordHash { get; set; }
           public string? Role { get; set; }
           [Required]
-          public string FirstName { get; set; } = default!;
+          public string FirstName { get; set; } = string.Empty;
           [Required]
-          public string LastName { get; set; } = default!;
+          public string LastName { get; set; } = string.Empty;
           public string? Email { get; set; }
      }
 }

39a1b2462ff498ef33bd021157b73633fec6770c
jalpesh-vertex <jalpesh.nakum@vertexone.net>
2025-10-03 09:13:31 +0530

    changes for package update

--- BrainBuzz.web/Migrations/ApplicationDbContextModelSnapshot.cs
+++ BrainBuzz.web/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -16,7 +16,7 @@ namespace BrainBuzz.web.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "9.0.7")
+                .HasAnnotation("ProductVersion", "8.0.10")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
